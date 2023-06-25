26d75df83bde034f3f59ae97cb8b41ea8894b43b
LuboDimitrov <lubo9991@hotmail.com>
2023-06-25 10:08:48 +0200

    feat: app component code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'demoapp';
+  username: string = "";
+  greeting: string = "";
+
+  constructor(private http: HttpClient) {}
+
+  submit() {
+    this.http.post('https://v3dkv5bwfa.execute-api.us-east-1.amazonaws.com/test', { username: this.username })
+      .subscribe((response: any) => {
+        this.greeting = response.greeting;
+      });
+  }
 }
